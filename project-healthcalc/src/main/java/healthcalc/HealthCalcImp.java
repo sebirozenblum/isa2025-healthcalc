4843be33685e192d6c06dc471b8fa55abce8e70e
sebirozenblum <sebirozenblum22@gmail.com>
2025-02-25 14:18:44 +0100

    Implemented idealWeight and basalMetabolicRate methods

--- project-healthcalc/src/main/java/healthcalc/HealthCalcImp.java
+++ project-healthcalc/src/main/java/healthcalc/HealthCalcImp.java
@@ -5,12 +5,34 @@ public class HealthCalcImp implements HealthCalc {
 
     @Override
     public float idealWeight(int height, char gender) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        float IW;
+
+        if (gender == 'M') {
+            IW = height - 100 - (height - 150) / 4;
+        }
+
+        else {
+            IW = height - 100 - (height - 150) / (float) 2.5;
+        }
+
+        return IW;
+
     }
 
     @Override
     public float basalMetabolicRate(float weight, int height, int age, char gender) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        float BMR;
+
+        if (gender == 'M') {
+            BMR = (float) (88.362 + 13.397 * weight + 4.799 * height - 5.677 * age);
+        }
+
+        else {
+            BMR = (float) (447.593 + 9.247 * weight + 3.098 * height - 4.330 * age);
+        }
+
+        return BMR;
     }
 
 }
