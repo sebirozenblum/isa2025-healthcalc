988e536560eac98c29052133b21f964bd831be9a
sebirozenblum <sebirozenblum22@gmail.com>
2025-02-25 14:05:55 +0100

    Created HealthCalcImp.java

---
+++ project-healthcalc/src/main/java/healthcalc/HealthCalcImp.java
@@ -0,0 +1,16 @@
+package healthcalc;
+
+
+public class HealthCalcImp implements HealthCalc {
+
+    @Override
+    public float idealWeight(int height, char gender) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public float basalMetabolicRate(float weight, int height, int age, char gender) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
