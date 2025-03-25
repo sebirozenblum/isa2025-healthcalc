212e781727642d6a31600b342e1f1e3a5f726402
sebirozenblum <sebirozenblum22@gmail.com>
2025-03-25 19:10:50 +0100

    fix: finish BMR tab with Altura, Edad, Sexo and Peso

--- project-healthcalc/src/main/java/healthcalc/Vista/VistaImp.java
+++ project-healthcalc/src/main/java/healthcalc/Vista/VistaImp.java
@@ -10,7 +10,7 @@ public class VistaImp extends JFrame implements Vista {
 
     private JTabbedPane tabbedPane;
     private JPanel panelPrincipal, panelIW, panelBMR, panelButton, panelTitulo, panelSexoIW, panelButtonGroupIW, panelSexoBMR, panelButtonGroupBMR;
-    private JTextField textFieldNombre;
+    private JTextField textFieldPeso;
     private JLabel labelTitulo, labelSubtitulo, labelAlturaIW, labelSexoIW, labelAlturaBMR, labelSexoBMR, labelEdad, labelPeso, labelResultado;
     private JRadioButton radioButtonHombreIW, radioButtonMujerIW, radioButtonHombreBMR, radioButtonMujerBMR;
     private ButtonGroup buttonGroupSexoIW, buttonGroupSexoBMR;
@@ -22,16 +22,16 @@ public class VistaImp extends JFrame implements Vista {
 
         // Panel principal
         panelPrincipal = new JPanel();
-        panelPrincipal.setBackground(new Color(227, 164, 104));
+        panelPrincipal.setBackground(new Color(255, 222, 114, 255));
         panelPrincipal.setLayout(new BorderLayout(10, 10));
         this.add(panelPrincipal);
 
         panelTitulo = new JPanel();
         panelTitulo.setLayout(new BorderLayout(0, 0));
-        panelTitulo.setBackground(new Color(227, 164, 104));
+        panelTitulo.setBackground(new Color(255, 222, 114, 255));
         labelTitulo = new JLabel();
         labelTitulo.setText("HEALTH CALCULATOR");
-        labelTitulo.setFont(new Font("Chakra Petch", Font.BOLD, 28));
+        labelTitulo.setFont(new Font("Chakra Petch", Font.BOLD, 32));
         labelTitulo.setHorizontalAlignment(SwingConstants.CENTER);
         labelSubtitulo = new JLabel();
         labelSubtitulo.setText("Calculate your ideal weight (IW) and basal metabolic rate (BMR)");
@@ -111,22 +111,73 @@ public class VistaImp extends JFrame implements Vista {
         // Panel BMR
         panelBMR = new JPanel();
         panelBMR.setBackground(new Color(230, 230, 230));
+        panelBMR.setLayout(new GridLayout(4, 2));
         tabbedPane.addTab("BMR", panelBMR);
 
+        labelAlturaBMR = new JLabel();
+        labelAlturaBMR.setText("Altura (cm): ");
+        labelAlturaBMR.setHorizontalAlignment(SwingConstants.CENTER);
+        labelAlturaBMR.setFont(new Font("Chakra Petch", Font.PLAIN, 18));
+        panelBMR.add(labelAlturaBMR);
 
+        spinnerAlturaBMR = new JSpinner();
+        spinnerAlturaBMR.setModel(new SpinnerNumberModel(100, 80, 210, 1));
+        spinnerAlturaBMR.setFont(new Font("Chakra Petch", Font.PLAIN, 16));
+        panelBMR.add(spinnerAlturaBMR);
 
-        textFieldNombre = new JTextField();
-        //panelPrincipal.add(textFieldNombre, BorderLayout.CENTER);
+        labelEdad = new JLabel();
+        labelEdad.setText("Edad (años): ");
+        labelEdad.setHorizontalAlignment(SwingConstants.CENTER);
+        labelEdad.setFont(new Font("Chakra Petch", Font.PLAIN, 18));
+        panelBMR.add(labelEdad);
 
+        spinnerEdad = new JSpinner();
+        spinnerEdad.setModel(new SpinnerNumberModel(50, 1, 105, 1));
+        spinnerEdad.setFont(new Font("Chakra Petch", Font.PLAIN, 16));
+        panelBMR.add(spinnerEdad);
+
+        labelSexoBMR = new JLabel();
+        labelSexoBMR.setText("Sexo: ");
+        labelSexoBMR.setFont(new Font("Chakra Petch", Font.PLAIN, 18));
+        labelSexoBMR.setHorizontalAlignment(SwingConstants.CENTER);
+        panelBMR.add(labelSexoBMR);
+
+        buttonGroupSexoBMR = new ButtonGroup();
+        radioButtonHombreBMR = new JRadioButton("Hombre");
+        radioButtonHombreBMR.setFont(new Font("Chakra Petch", Font.PLAIN, 16));
+        buttonGroupSexoBMR.add(radioButtonHombreBMR);
+        radioButtonMujerBMR = new JRadioButton("Mujer");
+        radioButtonMujerBMR.setFont(new Font("Chakra Petch", Font.PLAIN, 16));
+        buttonGroupSexoBMR.add(radioButtonMujerBMR);
+
+        panelButtonGroupBMR = new JPanel();
+        panelButtonGroupBMR.setLayout(new FlowLayout());
+        panelButtonGroupBMR.setBackground(new Color(230, 230, 230));
+        panelButtonGroupBMR.add(radioButtonHombreBMR);
+        panelButtonGroupBMR.add(radioButtonMujerBMR);
+        panelSexoBMR = new JPanel();
+        panelSexoBMR.setLayout(new BorderLayout(0, 0));
+        panelSexoBMR.setBackground(new Color(230, 230, 230));
+        panelSexoBMR.add(panelButtonGroupBMR, BorderLayout.CENTER);
+        panelBMR.add(panelSexoBMR);
+
+        labelPeso = new JLabel();
+        labelPeso.setText("Peso (kg): ");
+        labelPeso.setFont(new Font("Chakra Petch", Font.PLAIN, 18));
+        labelPeso.setHorizontalAlignment(SwingConstants.CENTER);
+        panelBMR.add(labelPeso);
+
+        textFieldPeso = new JTextField();
+        textFieldPeso.setFont(new Font("Chakra Petch", Font.PLAIN, 16));
+        textFieldPeso.setHorizontalAlignment(SwingConstants.CENTER);
+        panelBMR.add(textFieldPeso);
+
+        // Configuraciones
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500, 500);
         this.pack();
-        this.setTitle("HealthCalc");
+        this.setTitle("Health Calculator");
         this.setVisible(true);
-    }
-
-    public String getInputValue() {
-        return this.textFieldNombre.getText();
     }
 
     public void mostrarTexto(String mensaje) {
